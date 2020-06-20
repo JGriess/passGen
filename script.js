b4cadef48a2a8e703d1efe25a637da8a9d7811c2
JGries <jacksongriessen@gmail.com>
2020-06-19 23:56:09 -0500

    completed project

--- script.js
+++ script.js
@@ -3,31 +3,67 @@ var generateBtn = document.querySelector("#generate");
 
 var passLength;
 var passCase = Boolean;
-var passLower = Boolean;
+  var passLower = Boolean;
+var passNumbers = Boolean;
+var passSpecials = Boolean;
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
+var numberArray = ['1','2','3','4','5','6','7','8','9','0' ];
+var specialArray = ['!','@','#','$','%','^','&','*' ];
+var completeArray = "";
+var password = "";
+
 // Write password to the #password input
 
-//  passLength =  prompt
+
 function writePassword() {
   alert("Please select password criteria:")
   chooseLength()
   chooseCase()
-  confirm("Include numbers?")
-  confirm("Include special characters? (!@#$%^&*)") 
+  console.log(passCase)
+  console.log(passLower)
+  console.log(upperCaseArray)
+  passNum = confirm("Include numbers?")
+  passSpecial = confirm("Include special characters? (!@#$%^&*)")
+  if (passCase = true){
+    completeArray = completeArray = lowerCaseArray + upperCaseArray;
+  } 
+  if (passLower == true){
+    completeArray = completeArray + lowerCaseArray;
+  }
+  else {
+    completeArray = completeArray + upperCaseArray;
+  }
+  if (passNumbers = true){
+    completeArray = completeArray + numberArray;
+  }
+  if (passSpecials = true){
+    completeArray = completeArray + specialArray;
+  } 
+  console.log(completeArray)
+  generatePassword()
+  console.log(password)
+  alert("Your password is:"+ password)
 }
 
+//generates password by adding a random character from the complete array
+function generatePassword(){
+  for(i = 0; i < passLength; i++) {
+
+    var randomCharacter = Math.floor(Math.random() * completeArray.length);
+    password = password + completeArray[randomCharacter];
+  }
+}  
+
 function chooseLength() {
-  prompt("Choose number of characters in password. (8-128)");
-    if (passLength != Number){
+ passLength = parseInt(prompt("Choose number of characters in password. (8-128)")); 
+ console.log(passLength)
+    if (passLength < 8 || passLength > 128 || Number.isNaN(passLength) == true ){
       alert("Please imput a number between 8 and 128")
       chooseLength()
     }
-    passLength = Number
-    if (passLength < 8 ){
-      alert("Please imput a number between 8 and 128")
-      chooseLength()
-    }
-    else{
-    }
+
+    
 }
 function chooseCase(){
   passCase = confirm("Include both uppercase and lowercase characters?");
@@ -39,6 +75,7 @@ function chooseCase(){
     }  
 }
 
+
   //var password = generatePassword();
   //var passwordText = document.querySelector("#password");
 
@@ -47,4 +84,4 @@ function chooseCase(){
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", writePassword); 
